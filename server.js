6bfd292bb2242fef2129bd9d55197c016d89fbb4
Tomek Maszkowski <tomazy@go2.pl>
2015-01-27 16:59:08 +0800

    Updated log

--- server.js
+++ server.js
@@ -71,5 +71,5 @@ var server = app.listen(process.env.PORT || DEFAULT_PORT, function(err){
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('screenshot server listening at http://%s:%s', host, port);
 });
